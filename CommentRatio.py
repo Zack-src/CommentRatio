4492eb3f2276bae9f518d3ea731875ec2b30ce3d
Zack-src <theyloors@gmail.com>
2023-12-27 20:16:45 +0100

    update 1.2
    Fix folder not found / empty
    Fix color for somes env
    Add config.json

--- CommentRatio.py
+++ CommentRatio.py
@@ -1,68 +1,25 @@
 import os
 import sys
+import json
 import argparse
+import colorama
 
-# Symboles de commentaires de ligne
-LINE_SIMPLE = '//'
-LINE_HASH = '#'
-LINE_DASH = '--'
+def load_config(config_file_path):
+    with open(config_file_path, 'r') as config_file:
+        return json.load(config_file)
 
-# Symboles de commentaires de bloc
-BLOCK_START_SIMPLE = '/*'
-BLOCK_END_SIMPLE = '*/'
-BLOCK_START_HTML = '<!--'
-BLOCK_END_HTML = '-->'
-
-
-comment_symbols_map = {
-    '.c': {
-        'line': LINE_SIMPLE,
-        'block': [BLOCK_START_SIMPLE, BLOCK_END_SIMPLE]
-    },
-    '.cpp': {
-        'line': LINE_SIMPLE,
-        'block': [BLOCK_START_SIMPLE, BLOCK_END_SIMPLE]
-    },
-    '.h': {
-        'line': LINE_SIMPLE,
-        'block': [BLOCK_START_SIMPLE, BLOCK_END_SIMPLE]
-    },
-    '.hpp': {
-        'line': LINE_SIMPLE,
-        'block': [BLOCK_START_SIMPLE, BLOCK_END_SIMPLE]
-    },
-    '.cu': {
-        'line': LINE_SIMPLE,
-        'block': [BLOCK_START_SIMPLE, BLOCK_END_SIMPLE]
-    },
-    '.py': {
-        'line': LINE_HASH,
-        'block': None
-    },
-    '.html': {
-        'line': None,
-        'block': [BLOCK_START_HTML, BLOCK_END_HTML]
-    },
-    '.css': {
-        'line': None,
-        'block': [BLOCK_START_SIMPLE, BLOCK_END_SIMPLE]
-    },
-    '.js': {
-        'line': LINE_SIMPLE,
-        'block': [BLOCK_START_SIMPLE, BLOCK_END_SIMPLE]
-    },
-    '.sql': {
-        'line': LINE_DASH,
-        'block': [BLOCK_START_SIMPLE, BLOCK_END_SIMPLE]
-    },
-}
+def get_comment_symbols(extension, config):
+    comment_info = config["comment_symbols_map"].get(extension, {})
+    line_symbol_key = comment_info.get('line')
+    block_symbols_key = comment_info.get('block')
+    line_symbol = config["line_comment_symbols"].get(line_symbol_key) if line_symbol_key else None
+    block_symbols = [config["block_comment_symbols"][block_symbols_key][0], config["block_comment_symbols"][block_symbols_key][1]] if block_symbols_key else None
+    return line_symbol, block_symbols
 
 def count_comments(file_path):
     extension = os.path.splitext(file_path)[1].lower()
-    comment_info = comment_symbols_map.get(extension, {})
-    line_symbol = comment_info.get('line')
-    block_symbols = comment_info.get('block')
-    
+    line_symbol, block_symbols = get_comment_symbols(extension, config)
+
     in_block_comment = False
     comment_chars = 0
     total_chars = 0
@@ -94,8 +51,6 @@ def count_comments(file_path):
 
     return comment_chars, total_chars
 
-
-
 def calculate_comment_percentage(file_paths, min_ratio):
     max_length = max(len(path) for path in file_paths)
 
@@ -111,37 +66,43 @@ def calculate_comment_percentage(file_paths, min_ratio):
 
         file_name = file_path.ljust(max_length)
 
-        color = "\033[92m" if comment_ratio >= min_ratio else (
-            "\033[93m" if min_ratio - 5 <= comment_ratio <= min_ratio + 5 else "\033[91m")
+        color = "\033[92m" if comment_ratio >= min_ratio else ("\033[93m" if min_ratio - 5 <= comment_ratio <= min_ratio + 5 else "\033[91m")
         print(f"File: {file_name} | Comment Percentage: {color}{comment_ratio:.2f}%\033[0m")
 
     if total_chars > 0:
         total_comment_ratio = total_comment_chars / total_chars * 100
-        color = "\033[92m" if total_comment_ratio >= min_ratio else (
-            "\033[93m" if min_ratio - 5 <= total_comment_ratio <= min_ratio + 5 else "\033[91m")
+        color = "\033[92m" if total_comment_ratio >= min_ratio else ("\033[93m" if min_ratio - 5 <= total_comment_ratio <= min_ratio + 5 else "\033[91m")
         print(f"Global Comment Percentage: {color}{total_comment_chars / total_chars * 100:.2f}%\033[0m")
     else:
         print("No files found or files are empty.")
 
-
-def find_files(root_folder):
+def find_files(root_folder, config):
     found_files = []
     script_file = os.path.abspath(sys.argv[0])
 
     for root, _, file_list in os.walk(root_folder):
         for file in file_list:
-            full_path =  os.path.join(root, file)
-            if full_path != script_file and os.path.splitext(file)[1].lower() in comment_symbols_map:
-                found_files.append(os.path.join(root, file))
+            full_path = os.path.join(root, file)
+            if full_path != script_file:
+                file_extension = os.path.splitext(file)[1].lower()
+                if file_extension in config["comment_symbols_map"]:
+                    found_files.append(full_path)
+
     return found_files
 
-
-
 if __name__ == "__main__":
+    colorama.init()
+    config = load_config('config.json')
+
     parser = argparse.ArgumentParser(description='Calculate Comment Percentage in Code')
     parser.add_argument('--path', type=str, default=os.getcwd(), help='Path to the project folder (default: current directory)')
     parser.add_argument('--ratio', type=float, default=30, help='Minimum acceptable comment percentage (default: 30)')
     args = parser.parse_args()
 
-    file_paths = find_files(args.path)
+    file_paths = find_files(args.path, config)
+
+    if not file_paths:
+        print("No matching file found or empty folder.")
+        sys.exit(1)
+
     calculate_comment_percentage(file_paths, args.ratio)
